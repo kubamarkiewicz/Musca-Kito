76345e7e47977da5aef30e7bcdbcf30bddd7242e
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2015-04-07 01:01:49 +0200

    DB, Router

--- index.php
+++ index.php
@@ -4,19 +4,14 @@
 
 // use App\Kuba;
 use MuscaKit\DB;
+use MuscaKit\Router;
 
 require_once 'protected/config.php';
 
+$db = new DB(HOST, USER, PASSWORD, DB_NAME);
+// $i18n = new Musca_I18n(LANGS, $db);
+// $template = new Musca_Smarty($i18n);
+// $config = new Musca_Config($db);
 
-
-	
-
-	$db = new DB(HOST, USER, PASSWORD, DB_NAME);
-/*	$i18n = new Musca_I18n(LANGS, $db);
-	$template = new Musca_Smarty($i18n);
-	$config = new Musca_Config($db);
-
-	$dispatcher = new Musca_Dispatcher($db, $i18n, $template, $config);
-	$dispatcher->ignite($_SERVER['REQUEST_URI']);
-
-*/
+$router = new Router($db, $i18n, $template, $config);
+$router->ignite($_SERVER['REQUEST_URI']);
